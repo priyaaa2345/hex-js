82720edb869cb6c083e0edc4ed82ee3f8b865b92
priyaaa2345 <apriyadharshiniarumugam@gmail.com>
2024-05-17 16:30:54 +0530

    js

---
+++ loops.js
@@ -0,0 +1,50 @@
+// console.log("Hi from loops..");
+// const marks=[40,50,60,70,40];
+
+
+// for(let i =0;i<marks.length;i++){
+//     console.log(marks[i]);
+// }
+
+// // other ways to write for loops 
+
+// console.log("for ...in");
+// for(let idx in marks){
+//     console.log(marks[idx]);
+// }
+
+// console.log("for ...of")
+// for(let idx in marks){
+//     console.log(marks[idx]);
+// }
+
+
+// for ...in
+
+const avenger={
+    name:"tony-Shark",
+    house: "😒",
+    networth: " aaa",
+    power: "x",
+    phrase: "love u 3000"
+};
+
+
+const details={
+    age:40,
+    power: 's'
+};
+// for(let key in avenger){
+//     console.log(key,avenger[key])
+// }
+
+// console.log(avenger.house);
+// console.log({...avenger, ...details}); //power in details gets stored(cos its last in console)
+// console.log(Object.keys(avenger)) //array
+// console.log(Object.values(avenger))
+
+// console.log(skill); //undefined
+
+// to solve this use default value
+
+
